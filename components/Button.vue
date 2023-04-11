3153ef163efc3a5738c9cb284ddc2145a9dc7af6
Alexandra Collins <andracolexa@gmail.com>
2023-04-11 15:48:57 +0100

    implement reusable button component

---
+++ components/Button.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: 'button'
+    }
+  }
+}
+</script>
+
+<template>
+  <button class="bg-[#47BFA3] rounded-lg	h-12 px-8 text-sm"><slot /></button>
+</template>
+
