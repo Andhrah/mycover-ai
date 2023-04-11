490f82f8d404c7e98064e54002cf31a7a7b92e40
Alexandra Collins <andracolexa@gmail.com>
2023-04-11 15:53:43 +0100

    implement navbar layout

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<script>
+import Button from '~/components/Button.vue'
+
+export default {
+  components: {
+    Button,
+  }
+}
+</script>
+
+<template>
+  <nav class="bg-[#FFFFFF] flex px-36 h-[5.5rem] items-center justify-between shadow-sm mb-px fixed w-screen">
+    <img src="~/assets/images/mycover_logo.png" alt="logo" class="h-10 w-[10.375rem]">
+    <ul class="flex justify-between w-[45%]">
+      <li><NuxtLink to="/" aria-label="Products">Products</NuxtLink></li>
+      <li><NuxtLink to="/about" aria-label="Solutions">Solutions</NuxtLink></li>
+      <li><NuxtLink to="/contact" aria-label="Developers">Contact</NuxtLink></li>
+      <li><NuxtLink to="/contact" aria-label="Company">Company</NuxtLink></li>
+      <li><NuxtLink to="/contact" aria-label="Learn">Learn</NuxtLink></li>
+    </ul>
+
+    <div class="flex justify-between w-[24%]">
+      <Button class="bg-[#F2F4F7] text-[#475467]">Login</Button>
+      <Button class="text-[#FFFFFF]">Get Started</Button>
+    </div>
+  </nav>
+  <slot />
+</template>
