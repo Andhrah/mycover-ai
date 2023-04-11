2444e6103aec977397cdeab5870eea9578ee07c2
Alexandra Collins <andracolexa@gmail.com>
2023-04-11 15:51:20 +0100

    add accessibility to the button

--- components/Button.vue
+++ components/Button.vue
@@ -10,6 +10,12 @@ export default {
 </script>
 
 <template>
-  <button class="bg-[#47BFA3] rounded-lg	h-12 px-8 text-sm"><slot /></button>
+  <button class="bg-[#47BFA3] rounded-lg h-12 px-8 text-sm"
+    aria-label="Click me to perform an action"
+    role="button"
+    tabindex="0"
+    title="Perform an action">
+    <slot />
+  </button>
 </template>
 
