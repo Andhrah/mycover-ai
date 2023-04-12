7fce68b5438ab2b4164fb2c8b2c32a4619b8265d
Alexandra Collins <andracolexa@gmail.com>
2023-04-12 06:34:55 +0100

    implement app font family

--- app.vue
+++ app.vue
@@ -2,6 +2,10 @@
 import 'remixicon/fonts/remixicon.css'
 </script>
 
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap');
+</style>
+
 <template>
   <div>
     <NuxtLayout>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Space Grotesk', 'Helvetica', 'Arial', 'sans-serif'],
+        spaceGrotesk: ['"Space Grotesk"'],
+      },
+    },
+  },
+  plugins: [],
+}
+
